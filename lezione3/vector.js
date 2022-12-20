0249917624475f24ec86a134e72f58595d267bda
mdm13 <mario.demenech@gmail.com>
2022-12-20 08:50:29 +0100

    lezione 3

---
+++ lezione3/vector.js
@@ -0,0 +1,19 @@
+function sumArray(v) {
+    
+    var sum=0
+    for(var i=0;i<v.length;i++) {
+        sum+=v[i]
+    }
+
+    return sum
+}
+
+function concatWords(v) {
+    
+    var sum=''
+    for(var i=0;i<v.length;i++) {
+        sum+=v[i]
+    }
+
+    return sum
+}
