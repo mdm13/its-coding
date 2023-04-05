3b7b57fcbaa44c45e2786d976c76d5abad74f018
mdm13 <mario.demenech@gmail.com>
2023-04-05 08:35:03 +0200

    commenti

--- lezione14/global_local.c
+++ lezione14/global_local.c
@@ -5,20 +5,20 @@ variabili locali e globali
 *******************************************************************************/
 #include <stdio.h>
 
-int i=10;
+int i=10;  //variabile globale: definita fuori di qualsiasi funzione
 
 
 void fun1() {
-    i++;
+    i++;    //richiamiamo una variabile globale all'interno di una funzione
 }
 
 void fun2() {
-    int i=0;
+    int i=0;  //variabile locale con durata automatica: definita all'interno di una funzione
     i++;
 }
 
 void fun3() {
-    static int i=1;
+    static int i=1;   //variabile locale con durata fissa (statica): inizializzata all'avvio del programma e non all'inizio della funzione
     printf("static int = %d\n",i);
     
     i++;
@@ -36,6 +36,15 @@ int main()
     
     fun3();
     fun3();
+    
+    {
+        int i=50;
+        printf("i di blocco=%d\n",i);
+    }
+    {
+        printf("i globale = %d\n",i);
+    }   
+    
     
     return 0;
 }
